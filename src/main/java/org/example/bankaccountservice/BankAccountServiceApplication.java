357c867a89940cd3df8296a0f954fbe2e18fb0c1
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-10-17 23:21:15 +0100

    deuxieme commit

--- src/main/java/org/example/bankaccountservice/BankAccountServiceApplication.java
+++ src/main/java/org/example/bankaccountservice/BankAccountServiceApplication.java
@@ -22,7 +22,7 @@ public class BankAccountServiceApplication {
     CommandLineRunner start(BankAccountRepository bankAccountRepository){
         return args -> {
             for (int i = 0; i < 10; i++){
-                BankAccount bankAccount = BankAccount.builder()
+                BankAccount bankAccount = BankAccount.builder()//pattern builder
                         .id(UUID.randomUUID().toString())
                         .type(Math.random()>0.5? AccountType.CURRENT_ACCOUNT:AccountType.SAVING_ACCOUNT)
                         .balance(10000+Math.random()*90000)
---
+++ src/main/java/org/example/bankaccountservice/web/AccountRestController.java
@@ -0,0 +1,43 @@
+package org.example.bankaccountservice.web;
+
+import org.example.bankaccountservice.entities.BankAccount;
+import org.example.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class AccountRestController {
+    private BankAccountRepository bankAccountRepository;
+
+    public AccountRestController(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    @GetMapping("/bankAccounts")
+    public List<BankAccount> bankAccounts() {
+        return bankAccountRepository.findAll();
+    }
+
+    @GetMapping("/bankAccounts/{id}")
+    public BankAccount bankAccounts(@PathVariable String id) {
+        return bankAccountRepository.findById(id).orElseThrow(() -> new RuntimeException(String.format("Account with id %s not found", id)));
+
+    }
+
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount) {
+        return bankAccountRepository.save(bankAccount);
+    }
+
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount) {
+        BankAccount account=bankAccountRepository.findById(id).orElseThrow();
+        if (bankAccount.getBalance()!=null) account.setBalance(bankAccount.getBalance());
+        if (bankAccount.getCreatedAt()!=null) account.setCreatedAt(new Date());
+        if (bankAccount.getType()!=null) account.setType(bankAccount.getType());
+        if (bankAccount.getCurrency()!=null) account.setCurrency(bankAccount.getCurrency());
+        return bankAccountRepository.save(account);
+    }
+}
