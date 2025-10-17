be19c649c61b3a9dd62d96e2db40e6064ecc289d
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-10-17 22:13:46 +0100

    first commit

---
+++ src/main/java/org/example/bankaccountservice/enums/AccountType.java
@@ -0,0 +1,6 @@
+package org.example.bankaccountservice.enums;
+
+public enum AccountType {
+    CURRENT_ACCOUNT,
+    SAVING_ACCOUNT,
+}
---
+++ src/main/java/org/example/bankaccountservice/repositories/BankAccountRepository.java
@@ -0,0 +1,7 @@
+package org.example.bankaccountservice.repositories;
+
+import org.example.bankaccountservice.entities.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+}
